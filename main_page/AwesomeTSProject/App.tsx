919c479eb6a8761fd606753e8dce425c24c88f7b
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-05-15 20:22:56 +1200

    Removed unused imports

--- main_page/AwesomeTSProject/App.tsx
+++ main_page/AwesomeTSProject/App.tsx
@@ -1,17 +1,3 @@
-import React from 'react';
-import {
-  Alert,
-  Button,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import FlatButton from './src/assests/theme/styles';
 import MainMenuScreen from './src/screens/MainMenu.Screen';
 
 const App = () => {
