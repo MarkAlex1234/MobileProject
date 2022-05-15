ed6c952c60ccc9d5d4ec8cc20165669f4a6dfe1f
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-05-15 17:48:50 +1200

    Update main

--- main_page/AwesomeTSProject/App.tsx
+++ main_page/AwesomeTSProject/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {
+  Alert,
+  Button,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -11,10 +14,44 @@ import {
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>TEST</Text>
+    <SafeAreaView style={{backgroundColor: 'purple', flex: 1}}>
+      <Image
+        style={{
+          resizeMode: 'contain',
+        }}
+        source={require('./src/assests/images/logo.png')}
+      />
+      <View style={styles.seperator1}>
+        <Button title="PLAY" onPress={() => Alert.alert('WIP')} />
+        <Button title="SELECT VARIENT" onPress={() => Alert.alert('WIP')} />
+        <Button title="UPGRADES" onPress={() => Alert.alert('WIP')} />
+        <Button title="SETTINGS" onPress={() => Alert.alert('WIP')} />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  seperator1: {
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+  },
+});
 
 export default App;
