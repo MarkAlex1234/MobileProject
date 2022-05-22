583b0b64fdefe9ed78b545274f7a7ef83e4d58a6
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-05-22 18:34:51 +1200

    Updates to SelectVarient.Screen

--- main_page/AwesomeTSProject/src/screens/SelectVarient.Screen.tsx
+++ main_page/AwesomeTSProject/src/screens/SelectVarient.Screen.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from 'react-native';
 import FlatButton from '../assests/theme/flatButton';
 import styles from '../assests/theme/styles';
 
@@ -7,6 +14,46 @@ export default function SelectVarientScreen({navigation}) {
   return (
     <SafeAreaView style={{backgroundColor: 'purple', flex: 1}}>
       <View>
+        <TouchableOpacity onPress={() => Alert.alert('WIP')}>
+          <Image
+            style={{
+              resizeMode: 'center',
+            }}
+            source={require('../assests/images/variant1.jpg')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert('WIP')}>
+          <Image
+            style={{
+              resizeMode: 'center',
+            }}
+            source={require('../assests/images/variant2.jpg')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert('WIP')}>
+          <Image
+            style={{
+              resizeMode: 'center',
+            }}
+            source={require('../assests/images/variant3.jpg')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert('WIP')}>
+          <Image
+            style={{
+              resizeMode: 'center',
+            }}
+            source={require('../assests/images/variant4.jpg')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert('WIP')}>
+          <Image
+            style={{
+              resizeMode: 'center',
+            }}
+            source={require('../assests/images/variant5.jpg')}
+          />
+        </TouchableOpacity>
         <FlatButton
           text="Return"
           onPress={() => navigation.navigate('Main Menu')}
