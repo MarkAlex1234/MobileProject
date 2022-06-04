4f6419180fccb7da1abe44bcecf4751b9e4c77f7
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-06-04 13:43:27 +1200

    BugFixes

--- main_page/AwesomeTSProject/src/screens/Game.Screen.tsx
+++ main_page/AwesomeTSProject/src/screens/Game.Screen.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import {GameEngine} from 'react-native-game-engine';
 import FlatButton from '../assests/theme/flatButton';
-import entities from '../entities/index';
 
 export default function Game({navigation}) {
   return (
     <SafeAreaView style={{backgroundColor: 'purple', flex: 1}}>
       <View>
         <GameEngine
-          entities={entities()}
           style={{
             position: 'absolute',
             top: 0,
             right: 0,
             left: 0,
             bottom: 0,
-          }}>
-          <FlatButton
-            text="Return"
-            onPress={() => navigation.navigate('Main Menu')}
-          />
-        </GameEngine>
+          }}></GameEngine>
       </View>
     </SafeAreaView>
   );
