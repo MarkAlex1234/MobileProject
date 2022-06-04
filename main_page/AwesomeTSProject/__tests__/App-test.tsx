844157edc67cccd764535af879c0196603d6b708
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-06-05 11:48:42 +1200

    Update App-test.tsx

--- main_page/AwesomeTSProject/__tests__/App-test.tsx
+++ main_page/AwesomeTSProject/__tests__/App-test.tsx
@@ -1,14 +1,30 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Game from '../src/screens/Game.Screen';
+import MainMenu from '../src/screens/MainMenu.Screen';
+import SettingsScreen from '../src/screens/Settings.Screen';
+import SelectVarientScreen from '../src/screens/SelectVarient.Screen';
+import Upgrades from '../src/screens/Upgrades.Screen';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('Game Screen is called when clicked', () => {
+  expect(Game).toBeCalled;
+});
+
+test('Main Menu Screen is called when clicked', () => {
+  expect(MainMenu).toBeCalled;
+});
+
+test('Settings Screen is called when clicked', () => {
+  expect(SettingsScreen).toBeCalled;
+});
+
+test('Select Varient Screen is called when clicked', () => {
+  expect(SelectVarientScreen).toBeCalled;
+});
+
+test('Upgrades Screen is called when clicked', () => {
+  expect(Upgrades).toBeCalled;
 });
