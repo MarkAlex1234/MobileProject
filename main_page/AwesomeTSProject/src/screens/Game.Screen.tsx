00a2994b879789b22a22d8534a0d31ef0eb4b3c7
MarkAlex1234 <mark.alexander1314@gmail.com>
2022-06-04 12:40:24 +1200

    Added Variant Component

--- main_page/AwesomeTSProject/src/screens/Game.Screen.tsx
+++ main_page/AwesomeTSProject/src/screens/Game.Screen.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import {GameEngine} from 'react-native-game-engine';
 import FlatButton from '../assests/theme/flatButton';
 
 export default function Game({navigation}) {
   return (
     <SafeAreaView style={{backgroundColor: 'purple', flex: 1}}>
       <View>
-        <FlatButton
-          text="Return"
-          onPress={() => navigation.navigate('Main Menu')}
-        />
+        <GameEngine
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}>
+          <FlatButton
+            text="Return"
+            onPress={() => navigation.navigate('Main Menu')}
+          />
+        </GameEngine>
       </View>
     </SafeAreaView>
   );
